852462bb5a2236b4ed702209748d588d86b52c8c
Garjilius <emanuelegar@gmail.com>
2020-01-07 20:00:40 +0100

    name changes

--- src/main/java/com/emanuelegargiulo/calcolaobiettivomedia/CampoVuotoException.java
+++ src/main/java/com/emanuelegargiulo/calcolaobiettivomedia/CampoVuotoException.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.emanuelegargiulo.calcolavoto;
+package com.emanuelegargiulo.calcolaobiettivomedia;
 
 import javax.swing.JOptionPane;
 
